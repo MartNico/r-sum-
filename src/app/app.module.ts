8a3b1b705a92b9405da0c1369fa76fccef2d473f
Martin Nicolas <martin.nicolas@Martins-MacBook-Pro.local>
2017-12-03 17:00:33 -0500

    Setuping Angular Material and playing with it ! :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,38 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+//ANGULAR MATERIAL
+import {
+  MatButtonModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule
+} from '@angular/material';
+
+//COMPONENTS
+import { FrontpageComponent } from './frontpage/frontpage.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { MyexperiencesComponent } from './myexperiences/myexperiences.component';
+import { StudiesComponent } from './studies/studies.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontpageComponent,
+    AboutmeComponent,
+    MyexperiencesComponent,
+    StudiesComponent,
+    HobbiesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/myexperiences/myexperiences.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-myexperiences',
+  templateUrl: './myexperiences.component.html',
+  styleUrls: ['./myexperiences.component.css']
+})
+export class MyexperiencesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
